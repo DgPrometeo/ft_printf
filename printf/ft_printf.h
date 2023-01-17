ae79f105c5af76cf3e8ae444d6dc336abfffbdca
Daniel Garcia Sanchez <danielga@c2r1s3.42malaga.com>
2023-01-17 13:43:49 +0100

    fix error library

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:01:40 by danielga          #+#    #+#             */
-/*   Updated: 2023/01/17 12:04:27 by danielga         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:40:34 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
+# include <stdarg.h>
 
 int	ft_printf(char const *str, ...);
+int	ft_printchar(int c);
+int	ft_strchar(char *str);
 
 #endif
