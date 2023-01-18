6c4e3816b134fa9586e7ba985c0df8cf3f3cdf78
Daniel Garcia Sanchez <danielga@c2r1s3.42malaga.com>
2023-01-18 20:29:36 +0100

    delete commit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 12:50:01 by danielga          #+#    #+#             */
-/*   Updated: 2023/01/18 15:25:51 by danielga         ###   ########.fr       */
+/*   Updated: 2023/01/18 20:25:25 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,15 @@ static int	ft_variable(va_list args, char const str)
 	else if (str == 's')
 		len += ft_printstr(va_arg(args, char*));
 /*	else if (str == 'p')
-	//el puntero void * dado como argumento se imprime hexadecimalmente.*/
-	else if (str == 'd')
-		len += ft_printnbr(va_arg(args, int));
-	else if (str == 'i')
+	el puntero void * dado como argumento se imprime hexadecimalmente.*/
+	else if (str == 'd' || str == 'i')
 		len += ft_printnbr(va_arg(args, int));
 /*	else if (str == 'u')
-	//imprime un numero decimal sin signo.
+	imprime un numero decimal sin signo.
 	else if (str == 'x')
-	//imprime un número hexadecimal (base 16) en minusculas
+	imprime un número hexadecimal (base 16) en minusculas
 	else if (str == 'X')
-		//imprime un número hexadecimal (base 16) en mayusculas */
+	imprime un número hexadecimal (base 16) en mayusculas */
 	else if (str == '%')
 		len += ft_printchar('%');
 	return (len);
