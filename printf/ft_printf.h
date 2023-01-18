51585ad2fe3b65bb6aef90e7dbaa037785318cb4
Daniel Garcia Sanchez <danielga@c2r1s3.42malaga.com>
2023-01-18 15:54:41 +0100

    add new function

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:01:40 by danielga          #+#    #+#             */
-/*   Updated: 2023/01/17 15:30:00 by danielga         ###   ########.fr       */
+/*   Updated: 2023/01/18 15:07:46 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int	ft_printf(char const *str, ...);
-int	ft_printchar(int c);
-int	ft_strprint(char *str);
+int		ft_printf(char const *str, ...);
+int		ft_printchar(int c);
+int		ft_printstr(char *str);
+int		ft_printnbr(int n);
 
 #endif
