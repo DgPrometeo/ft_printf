5c77a5180ea1b235db3dcc7a8a7ef9e1a37bdc43
Daniel Garcia Sanchez <danielga@c3r4s3.42malaga.com>
2023-01-20 19:02:07 +0100

    News functions

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:01:40 by danielga          #+#    #+#             */
-/*   Updated: 2023/01/20 18:00:12 by danielga         ###   ########.fr       */
+/*   Updated: 2023/01/20 18:27:18 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_printf(char const *str, ...);
 int		ft_printchar(int c);
 int		ft_printstr(char *str);
 int		ft_printnbr(int n);
-// int		ft_printhex(void *p);
+int		ft_printhex(unsigned int p);
 int		ft_print_unsig_nbr(unsigned int n);
 
 #endif
