696aafdd7d00cfeeeb2c69167f144a6dd64ed448
DgPrometeo <danielgarciasan93@gmail.com>
2023-01-31 17:33:29 +0100

    Añadidas las otras opciones que faltaban de %
    Ya he añadido las últimas funciones que quedaban para las posibles
    variables que me podían dar. Pendiente de arreglar dos de ellas.

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 12:50:01 by danielga          #+#    #+#             */
-/*   Updated: 2023/01/20 19:00:07 by danielga         ###   ########.fr       */
+/*   Updated: 2023/01/31 17:29:36 by danielga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,21 @@ static int	ft_variable(va_list args, char const str)
 	else if (str == 's')
 		len += ft_printstr(va_arg(args, char *));
 	else if (str == 'p')
-		len += ft_printhex(va_arg(args, unsigned int));
+	{
+		len += ft_printstr("0x");
+		len += ft_printhex(va_arg(args, unsigned long long),
+				"0123456789abcdef");
+	}
 	else if (str == 'd' || str == 'i')
 		len += ft_printnbr(va_arg(args, int));
 	else if (str == 'u')
-	len += ft_print_unsig_nbr(va_arg(args, unsigned int));
+		len += ft_print_unsig_nbr(va_arg(args, unsigned int));
 	else if (str == 'x')
-	len += ft_printhex(va_arg(args, unsigned int));
+		len += ft_printhex((unsigned long long)
+				va_arg(args, unsigned long), "0123456789abcdef");
 	else if (str == 'X')
-	len += ft_printhex(va_arg(args, unsigned int));
+		len += ft_printhex((unsigned long long)
+				va_arg(args, unsigned long), "0123456789ABCDEF");
 	else if (str == '%')
 		len += ft_printchar('%');
 	return (len);
